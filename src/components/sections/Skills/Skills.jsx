9788555fe534b0abd62609247dfa678b94e14dec
tahkasai <tainakasai01@gmail.com>
2025-11-23 15:14:05 -0300

    refactor: Adiciona mais habilidades técnicas e Remove pessoais

--- src/components/sections/Skills/Skills.jsx
+++ src/components/sections/Skills/Skills.jsx
@@ -9,57 +9,62 @@ export default function Skills(){
             <p>Afinidade com as seguintes tecnologias e técnicas:</p>
             <div className={styles.habilidades}>
                 <div className={styles.habTecnicas}>
-                    <h3>Técnicas (Hard Skills)</h3>
-                    <div>
-                        <span className={styles.nomeHab}>HTML</span>
-                        <ProgressBar tamanho="95%"/>
+                    <div className={styles.gridHab}>
+                        <div>
+                            <span className={styles.nomeHab}>HTML</span>
+                            <ProgressBar tamanho="95%"/>
+                        </div>
+                        <div>
+                            <span className={styles.nomeHab}>CSS</span>
+                            <ProgressBar tamanho="90%"/>
+                        </div>
+                        <div>
+                            <span className={styles.nomeHab}>JavaScript</span>
+                            <ProgressBar tamanho="70%"/>
+                        </div>
+                        <div>
+                            <span className={styles.nomeHab}>React JS</span>
+                            <ProgressBar tamanho="80%"/>
+                        </div>
+                        <div>
+                            <span className={styles.nomeHab}>Java</span>
+                            <ProgressBar tamanho="75%"/>
+                        </div>
+                        <div>
+                            <span className={styles.nomeHab}>Python</span>
+                            <ProgressBar tamanho="60%"/>
+                        </div>
+                        <div>
+                            <span className={styles.nomeHab}>MySQL</span>
+                            <ProgressBar tamanho="70%"/>
+                        </div>
                     </div>
-                    <div>
-                        <span className={styles.nomeHab}>CSS</span>
-                        <ProgressBar tamanho="90%"/>
-                    </div>
-                    <div>
-                        <span className={styles.nomeHab}>JavaScript</span>
-                        <ProgressBar tamanho="70%"/>
-                    </div>
-                    <div>
-                        <span className={styles.nomeHab}>PHP</span>
-                        <ProgressBar tamanho="70%"/>
-                    </div>
-                    <div>
-                        <span className={styles.nomeHab}>Java</span>
-                        <ProgressBar tamanho="75%"/>
-                    </div>
-                    <div>
-                        <span className={styles.nomeHab}></span>
-                        <ProgressBar tamanho="10%"/>
-                    </div>
-                    <div>
-                        <span className={styles.nomeHab}></span>
-                        <ProgressBar tamanho="10%"/>
-                    </div>
-                </div>
-                <div className={styles.habPessoais}>
-                    <h3>Pessoais (Soft Skills)</h3>
-                    <div>
-                        <span className={styles.nomeHab}></span>
-                        <ProgressBar tamanho="10%"/>
-                    </div>
-                    <div>
-                        <span className={styles.nomeHab}></span>
-                        <ProgressBar tamanho="10%"/>
-                    </div>
-                    <div>
-                        <span className={styles.nomeHab}></span>
-                        <ProgressBar tamanho="10%"/>
-                    </div>
-                    <div>
-                        <span className={styles.nomeHab}></span>
-                        <ProgressBar tamanho="10%"/>
-                    </div>
-                    <div>
-                        <span className={styles.nomeHab}></span>
-                        <ProgressBar tamanho="10%"/>
+                    <h3>Ferramentas</h3>
+                    <div className={styles.gridHab}>
+                        <div>
+                            <span className={styles.nomeHab}>Figma</span>
+                            <ProgressBar tamanho="80%"/>
+                        </div>
+                        <div>
+                            <span className={styles.nomeHab}>Canva</span>
+                            <ProgressBar tamanho="95%"/>
+                        </div>
+                        <div>
+                            <span className={styles.nomeHab}>Git</span>
+                            <ProgressBar tamanho="65%"/>
+                        </div>
+                        <div>
+                            <span className={styles.nomeHab}>GitHub</span>
+                            <ProgressBar tamanho="80%"/>
+                        </div>
+                        <div>
+                            <span className={styles.nomeHab}>Node.js</span>
+                            <ProgressBar tamanho="50%"/>
+                        </div>
+                        <div>
+                            <span className={styles.nomeHab}>Spring Boot</span>
+                            <ProgressBar tamanho="35%"/>
+                        </div>
                     </div>
                 </div>
             </div>
