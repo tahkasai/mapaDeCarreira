28614cf2aead3f1bd57a34f8bdea18242be2f11a
tahkasai <tainakasai01@gmail.com>
2026-01-10 17:16:20 -0300

    feat: Integra EmailJS ao formulário de contato

--- src/components/sections/Contact/Contact.jsx
+++ src/components/sections/Contact/Contact.jsx
@@ -1,8 +1,46 @@
-import BotaoForms from './../../common/Button/Button';
 import styles from './Contact.module.css'
+import button from './../../common/Button/Button.module.css'
 import SplitText from './../../common/SplitText/SplitText';
+import { useState } from 'react';
+import emailjs from '@emailjs/browser'
 
 export default function Contact(){
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [assunto, setAssunto] = useState('')
+    const [mensagem, setMensagem] = useState('')
+
+    function enviandoEmail(e){
+        e.preventDefault();
+
+        if (nome === ''|| email === '' || assunto === '' || mensagem === ''){
+            alert("Preencha todos os campos");
+            return;
+        }
+
+        const template = {
+            nome: nome,
+            email: email,
+            assunto: assunto,
+            mensagem: mensagem
+        }
+
+        emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_I,
+                     import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+                     template,
+                     import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        )
+        .then((resposta) => {
+            console.log("Email enviado",resposta.status,resposta.text)
+            setNome('')
+            setAssunto('')
+            setMensagem('')
+            setEmail('')
+        }, (err) =>{
+            console.log("ERRO: ",err)
+        })
+    }
+
     return(
         <>
             <section className={styles.contato} id='contato'>
@@ -46,21 +84,51 @@ export default function Contact(){
                         </div>
                     </div>
                     <div className={styles.formsContato}>
-                        <form action="">
+                        <form action="" onSubmit={enviandoEmail}>
                             <label htmlFor="nome">Nome</label>
-                            <input type="text" name="nome" id="nome" placeholder="Digite seu nome" className={styles.inputContato} required/>
+                            <input 
+                                type="text" 
+                                name="nome" 
+                                id="nome" 
+                                placeholder="Digite seu nome" 
+                                className={styles.inputContato} 
+                                onChange={(e) => setNome(e.target.value)}
+                                value={nome}
+                            />
 
                             <label htmlFor="email">E-mail</label>
-                            <input type="email" name="email" id="email" placeholder="Digite seu e-mail" className={styles.inputContato} required/>
+                            <input 
+                                type="email" 
+                                name="email" 
+                                id="email" 
+                                placeholder="Digite seu e-mail" 
+                                className={styles.inputContato} 
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                            />
 
                             <label htmlFor="assunto">Assunto</label>
-                            <input type="text" name="assunto" id="assunto" placeholder="Digite o assunto" className={styles.inputContato} required/>
+                            <input 
+                                type="text" 
+                                name="assunto" 
+                                id="assunto" 
+                                placeholder="Digite o assunto" 
+                                className={styles.inputContato} 
+                                onChange={(e) => setAssunto(e.target.value)}
+                                value={assunto}
+                            />
 
                             <label htmlFor="mensagem">Mensagem</label>
-                            <textarea name="mensagem" id="mensagem" rows="5" className={styles.inputContato} placeholder='Digite o assunto' required></textarea>
-                            <BotaoForms
-                                texto="Enviar"
+                            <textarea 
+                                name="mensagem" 
+                                id="mensagem" 
+                                rows="5" 
+                                className={styles.inputContato} 
+                                placeholder='Digite o assunto' 
+                                onChange={(e) => setMensagem(e.target.value)}
+                                value={mensagem}
                             />
+                            <input type="submit" value="enviar" className={button.BotaoForms}/>
                         </form>
                     </div>
                 </div>
